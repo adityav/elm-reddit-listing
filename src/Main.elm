99d1382b0c6a61e98955dc16cfc59e9d1f86bc1e
Aditya <blackleopard92@gmail.com>
2016-07-28 23:21:06 +0530

    finishing up with reddit sample app

--- src/Main.elm
+++ src/Main.elm
@@ -1,63 +1,87 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.App as Html
+import Html.App as App
 import Html.Events exposing ( onClick )
+import Http
+import Task
+import Debug
+import TimeTravel.Html.App as TimeTravel
+import Json.Decode exposing (decodeValue)
 
--- component import example
-import Components.Hello exposing ( hello )
+-- components
+import Components.RedditWidget as RedditWidget 
+
+-- models
+import Models exposing (..)
+import Decoders
+-- views
+import Views exposing (..)
+
+-- msgs
+import Msgs exposing (..)
+
+-- port
+import AppPorts
 
 
 -- APP
-main : Program Never
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  TimeTravel.program 
+  --App.program
+  { view = view
+  , update = update
+  , init = init
+  , subscriptions = subscriptions}
 
 
--- MODEL
-type alias Model = Int
 
-model : number
-model = 0
-
+-- INIT
+init : (Model, Cmd Msg)
+init =
+    (Model emptyListing Nothing True, RedditWidget.loadRedditLinks)
 
 -- UPDATE
-type Msg = NoOp | Increment
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    NoOp -> model
-    Increment -> model + 1
+    Noop -> 
+      (model, Cmd.none)
+
+    FetchStarted url ->
+      { model | isLoading = True } ! []
+
+    RedditFetchSucceed redditData ->
+
+      { model |
+        redditListing = appendListing model.redditListing redditData,
+        isLoading = False } ! [ AppPorts.isLastElemVisible "reddit-listing" ]
+
+    RedditFetchFail err ->
+
+      ({ model |
+        fetchError = Just (Debug.log "Network Error" err),
+        isLoading = False }, Cmd.none)
+
+    LastItemVisible isVisible ->
+      if isVisible then 
+        ({ model | isLoading = True }, RedditWidget.loadMoreRedditLinks model.redditListing.after)
+      else 
+        (model, Cmd.none)
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
-view : Model -> Html Msg
-view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
+
+-- Subscriptions
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.batch
+  [ AppPorts.lastItemVisible LastItemVisible,
+    -- process jsonp Succeed messages.
+    -- here we are decoding the data and if it fails, returning an empty list
+    AppPorts.fetchJsonPSucceed 
+    <| decodeValue Decoders.decodeNews
+    >> Result.map RedditFetchSucceed
+    -->> (\r -> Debug.log "resultant" r)
+    >> (Result.withDefault <| RedditFetchSucceed emptyListing)
   ]
 
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,57 @@
 // pull in desired CSS/SASS files
 require( './styles/main.scss' );
+var jsonp = require('jsonp');
+
+function isElementInViewport (el) {
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
 
 // inject bundled Elm app into div#main
 var Elm = require( './Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
+var app = Elm.Main.embed( document.getElementById( 'main' ) );
+
+//
+// jsonp integration
+//
+
+app.ports.fetchJsonP.subscribe(function(url) {
+    jsonp(url, {
+        param : 'jsonp'
+    },function(err, data) {
+        if(err) {
+            return app.ports.fetchJsonPFail.send(err);
+        }
+        app.ports.fetchJsonPSucceed.send(data);
+    });
+});
+
+app.ports.isLastElemVisible.subscribe(function(divId) {
+    console.log("Setting up listener for last item with Id:", divId);
+    // scroll stop
+    window.onscroll = function () {
+      var wrapper = document.getElementById(divId);
+      var lastItem = wrapper.childNodes[wrapper.childNodes.length - 1];
+      // app.ports.lastItemVisible.send(false);
+      if (isElementInViewport(lastItem)) {
+        app.ports.lastItemVisible.send(true);
+        // remove handler
+        window.onscroll = null;
+      } else {
+        // app.ports.lastItemVisible.send(false);
+      }
+    };
+
+});
